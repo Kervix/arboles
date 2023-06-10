9381b9e9825da69018c1172ef90862dd17d0ba54
Kervix <casasmilton10@gmail.com>
2023-06-10 20:13:22 -0300

    avances

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "btree.h"
+
+static void imprimir_entero(int data) {
+  printf("%d ", data);
+}
+
+int main() {
+  BTree ll = btree_unir(1, btree_crear(), btree_crear());
+  BTree l = btree_unir(2, ll, btree_crear());
+  BTree r = btree_unir(3, btree_crear(), btree_crear());
+  BTree raiz = btree_unir(4, l, r);
+
+  int a;
+  btree_recorrer(raiz, BTREE_RECORRIDO_POST, imprimir_entero);
+  puts("");
+  a = btree_buscar(raiz, 3);
+  printf("%d\n", a);
+  btree_destruir(raiz);
+
+  return 0;
+}
